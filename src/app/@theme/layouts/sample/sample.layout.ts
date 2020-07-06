757c3a841f9ab49a50be64e61f0005d5a0320fe1
Nikola Stoilov <ns@auxcode.com>
2020-07-06 17:55:16 +0300

    initial commit with updated packages

--- src/app/@theme/layouts/sample/sample.layout.ts
+++ src/app/@theme/layouts/sample/sample.layout.ts
@@ -27,9 +27,6 @@ import { StateService } from '../../../@core/utils';
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive [end]="sidebar.id === 'end'">
         <nb-sidebar-header *ngIf="currentTheme !== 'corporate'">
-          <a href="#" class="btn btn-hero-success main-btn">
-            <i class="ion ion-social-github"></i> <span>Support Us</span>
-          </a>
         </nb-sidebar-header>
         <ng-content select="nb-menu"></ng-content>
       </nb-sidebar>
